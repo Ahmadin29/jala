70bc6a53faaa9551827a12b8d57f2d78d07a2b77
Ahmad Saefudin <ahmaddin211056@gmail.com>
2021-12-10 00:52:05 +0700

    app js customizition

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import Index from './App/Screens/index';
+import { StatusBar, View } from 'react-native';
+import MainController from './App/Controller/MainController';
+import { createStackNavigator } from '@react-navigation/stack';
+import useCachedResources from './App/Helper/setLoadResources';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+
+  const isLoadingComplete = useCachedResources();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={{
+      flex:1,
+    }} >
+      <StatusBar backgroundColor="white" translucent={false} barStyle="dark-content" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Root"
+            component={MainController}
+            options={{
+              headerShown:false
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
