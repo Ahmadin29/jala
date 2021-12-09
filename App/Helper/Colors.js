146e57454e05565f8f25ec4d269e161a790d3f84
Ahmad Saefudin <ahmaddin211056@gmail.com>
2021-12-10 00:54:29 +0700

    add colors helper

---
+++ App/Helper/Colors.js
@@ -0,0 +1,17 @@
+export default {
+    primaryColor:"#2F80EC",
+    warningColor:"#F1A104",
+    successColor:"#45D45C",
+    dangerColor:"#E33946",
+
+    textColor:"#505050",
+    secondaryTextColor:"#AAA",
+    secondaryTextColor2:"#777",
+    
+    grey1:"#C8C8C8",
+    grey2:"#F3F3F3",
+    grey3:"#FAFAFA",
+
+    whiteColor:"#FFFFFF",
+  };
+  
