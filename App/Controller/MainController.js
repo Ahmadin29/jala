01c5b701cce106bba05a35ad9f87e1b13e246c8f
Ahmad Saefudin <ahmaddin211056@gmail.com>
2021-12-12 00:11:00 +0700

    add pricelist and post detail routes and remove unused code

--- App/Controller/MainController.js
+++ App/Controller/MainController.js
@@ -3,14 +3,10 @@ import * as React from 'react';
 import { getFocusedRouteNameFromRoute } from "@react-navigation/core";
 import BotttomNavigator from './BottomController';
 import PriceDetail from '../Screens/prices/detail';
+import PostDetail from '../Screens/posts/detail';
+import PricesIndex from '../Screens/prices';
 
 const Stack = createStackNavigator();
-
-const getHeaderName = (route)=>{
-  const routeName = getFocusedRouteNameFromRoute(route);
-  console.log(routeName);
-	return routeName;
-}
 
 export default function MainController({ navigation, route }) {
   return (
@@ -23,10 +19,24 @@ export default function MainController({ navigation, route }) {
             }}
         />
         <Stack.Screen
+            name="Prices"
+            component={PricesIndex}
+            options={{
+              headerTitle:"Daftar Harga Udang",
+            }}
+        />
+        <Stack.Screen
             name="PriceDetail"
             component={PriceDetail}
             options={{
               headerTitle:"Detail Harga",
+            }}
+        />
+        <Stack.Screen
+            name="PostDetail"
+            component={PostDetail}
+            options={{
+              headerTitle:"Detail Posts",
             }}
         />
     </Stack.Navigator>
