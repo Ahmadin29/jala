f9a657a8361703ca19dc248410196f8a39d4d24a
Ahmad Saefudin <ahmaddin211056@gmail.com>
2021-12-12 12:20:17 +0700

    connect posts list component into post list screen

--- App/Components/Index/posts.js
+++ App/Components/Index/posts.js
@@ -1,14 +1,17 @@
 import * as React from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, TouchableOpacity, View } from 'react-native';
 import Colors from '../../Helper/Colors';
 import KabarIcon from '../../../assets/images/icon/kabar.svg';
 import Text from "../Text";
 import moment from 'moment';
 import SingleKabar from '../posts/single';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Posts(props) {
 
-    const [prices,setPrices] = React.useState();
+    const [posts,setPrices] = React.useState();
+
+    const navigation = useNavigation();
 
     React.useEffect(()=>{
         setPrices(props.data)
@@ -20,20 +23,31 @@ export default function Posts(props) {
                 padding:15,
                 paddingBottom:0,
                 flexDirection:"row",
-                alignItems:"center"
+                alignItems:"center",
+                justifyContent:"space-between"
             }} >
-                <KabarIcon width={20} height={20}/>
                 <View style={{
-                    marginLeft:8,
+                    flexDirection:"row",
+                    alignItems:"center"
                 }} >
-                    <Text weight="semi" size={18} >Kabar Udang</Text>
-                    {
-                        prices && <Text color={Colors.secondaryTextColor} size={13} >{moment(prices[prices.length - 1].date).format('DD MMM YYYY')} - {moment(prices[0].date).format('DD MMM YYYY')}</Text>
-                    }
+                    <KabarIcon width={20} height={20}/>
+                    <View style={{
+                        marginLeft:8,
+                    }} >
+                        <Text weight="semi" size={18} >Kabar Udang</Text>
+                        {
+                            posts && <Text color={Colors.secondaryTextColor} size={13} >{moment(posts[posts.length - 1].date).format('DD MMM YYYY')} - {moment(posts[0].date).format('DD MMM YYYY')}</Text>
+                        }
+                    </View>
                 </View>
+                <TouchableOpacity onPress={()=>{
+                    navigation.navigate('Posts')
+                }} >
+                    <Text weight="semi" color={Colors.primaryColor} >Lihat Semua</Text>
+                </TouchableOpacity>
             </View>
             <FlatList
-                data={prices}
+                data={posts}
                 renderItem={({item,index})=><SingleKabar data={item} />}
                 showsHorizontalScrollIndicator={false}
             />
